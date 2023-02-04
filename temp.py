ad25be3134e57881815727a471ba12dee408361d
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-02-04 17:51:48 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,28 +1,30 @@
-# def isPrime(x):
+def isPrime(x):
 
-#     i = 2
-#     while i * i <= x:
-#         if x % i == 0:
-#             return False
-#         i += 1
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
+            return False
+        i += 1
         
-#     return True
+    return True
 
 
-# while True:
+while True:
 
-#     n = int(input())
+    n = int(input())
 
-#     count = 0
-#     for i in range(n + 1, 2 * n + 1):
-#         if isPrime(i):
-#             count += 1
-#     print(count)
+    count = 0
+    for i in range(n + 1, 2 * n + 1):
+        if isPrime(i):
+            count += 1
+    print(count)
 
-#     if n == 0:
-#         break
+    if n == 0:
+        break
 
-a = 1
-b = 123456
-nRange = list(range(a, 2 * b + 1))
-print(nRange)
+# a = 1
+# b = 123456
+# nRange = list(range(a, 2 * b + 1))
+
+# for v in nRange:
+#     if isPrime(v)
